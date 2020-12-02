7a2cfad60348de41197cfe14a3259a0327ecdd2c
aaronwilliame <68785474+aaronwilliame@users.noreply.github.com>
2020-12-02 15:28:13 -0500

    needed htdocs
    created needed php code

---
+++ htdocs/index.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+</head>
+
+<body>
+
+<h1>RA DATABASE</h1>
+
+<?php
+
+include "db_connect.php";
+
+include "search_all_students.php";
+
+include "search_building_3.php";
+
+$mysqli->close();
+
+
+
+?> 
+
+
+
+</body>
+
+</html>
+
+
