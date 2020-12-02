47a4aba6bae58ba4adb6e0586b1a813cd2a9f322
aaronwilliame <68785474+aaronwilliame@users.noreply.github.com>
2020-12-02 16:56:14 -0500

    Created add_student.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
 <body>
 
-<h1>RA DATABASE</h1>
+<h1><center>RA DATABASE</center></h1>
 
 <?php
 
@@ -13,26 +13,65 @@ include "db_connect.php";
 
 include "search_all_students.php";
 
-
 ?>
 <br>
 	<br>
 	
 <form action="search_keyword.php">
-	Search for a student by first<br> <br>
+	<b>Search for a student by <i>FIRST</i> name</b><br> <br>
 	<input type="text" name="keyword">
-
+	
+	<br>
+		<br>
 	
 	<input type="submit" value="Submit">
 	
 </form>
 
+
+<hr>
+<form action="add_student.php">
+	<h3>Enter a new student:</h3><br><br>
+	
+	<b>Enter Student ID:</b><br>
+	<input type="text" name="studentid"><br><br>
+	
+	<b>Enter Student year:</b><br>
+	<input type="text" name="studentyear"><br><br>
+	
+	<b>Enter Student gender (M/F):</b><br>
+	<input type="text" name="studentgender"><br><br>
+	
+	<b>Enter Student email:</b><br>
+	<input type="text" name="studentemail"><br><br>
+	
+	<b>Enter Student room number:</b><br>
+	<input type="text" name="studentroom"><br><br>
+	
+	<b>Enter First Name:</b><br>
+	<input type="text" name="newfname"><br><br>
+
+	<b>Enter Last Name:</b> <br>
+	<input type="text" name="newlname"><br><br>
+	
+	<b>Enter Building ID:</b> <br>
+	<input type="text" name="newbuildingid"><br><br>
+	
+	<b>Enter RA ID:</b> <br>
+	<input type="text" name="newraid"><br><br>
+	
+	<br>
+		<br>
+	<input type="submit" value="Submit">
+	
+</form>
+
+
+
 <?php
 //include "search_keyword.php";
 
 $mysqli->close();
-
-
 
 ?> 
 
